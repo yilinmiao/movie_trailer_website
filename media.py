3f089df8bf56ae3728d61a5c4cae5a9bea0e0c95
yilinmiao <yi.matheson@gmail.com>
2017-04-20 14:29:37 -0700

    added comments for media.py

--- media.py
+++ media.py
@@ -4,6 +4,8 @@ class Movie():
     """ This class provides a way to store movie related information"""
     
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    #define constructor/initialization method for class movie
+    #constructor contains parameters: movie_title, box_art, trailer_link.
     def __init__(self, movie_title, box_art, trailer_link):
         self.title = movie_title
         self.poster_image_url = box_art
