677ed00f0527e22058a131df5062385f2ed41db1
yilinmiao <yi.matheson@gmail.com>
2018-01-18 19:33:22 -0800

    Added comments for movie class

--- media.py
+++ media.py
@@ -2,7 +2,18 @@ import webbrowser
 
 
 class Movie():
-    """ This class provides a way to store movie related information"""
+    """ This class provides a way to store movie related information
+
+    Args:
+        movie_title (str)
+        box_art (str): Movies' Posters.
+        trailer_link (str): Youtube Links.
+
+    Attributes:
+        movie_title (str)
+        box_art (str): Movies' Posters.
+        trailer_link (str): Youtube Links.
+    """
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     # define constructor/initialization method for class movie
